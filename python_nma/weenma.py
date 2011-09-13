cff80d3ccd418e14ec75c4c9ce6c5b24cbe26f82
Romain Chossart <romainchossart@gmail.com>
2011-09-13 16:14:03 +0200

    Add a variable to activate / deactivate the plugin

--- python_nma/weenma.py
+++ python_nma/weenma.py
@@ -1,42 +1,32 @@
 # Author: sitaktif <sitaktif AT gmail DOT com>
 # This plugin calls the pynma bindings via python when somebody says your nickname, sends you a query, etc.
-# To make it work, you may need to download: pynma.py from NotifyMyAndroid website.
 # Requires Weechat 0.3.0
+# To make it work, you need to get pynma.py from NotifyMyAndroid website or on github - https://github.com/uskr/pynma . Just put it in the same folder as the script)
 # Released under GNU GPL v2
 # Heavily based on lavaramano's script "notify.py" v. 0.0.5
 #
 # 2011-07-22, sitaktif
 #     version 0.0.1: initial release
 
-# SCRIPTING NOTES:
-#
-# Tags (most commonly used):
-#  no_filter, no_highlight, no_log, log0..log9 (log level),
-#  notify_none, notify_message, notify_private, notify_highlight,
-#  nick_xxx (xxx is nick in message),
-#  irc_xxx (xxx is command name or number, see /server raw),
-#  irc_numeric, irc_error, irc_action, irc_ctcp, irc_ctcp_reply, irc_smart_filter, away_info.
-#
-# Buffer local vars (list them):
-# /buffer localvar
-
 import weechat
 
-weechat.register("nma", "sitaktif", "0.0.1", "GPL", "nma: Receive notifications ", "", "")
+weechat.register("nma", "sitaktif", "0.1.0", "GPL", "nma: Receive notifications on NotifyMyAndroid app.", "", "")
 
 # script options
 settings = {
     "apikey"               : "",
-    "show_hilights"        : "on",
-    "hilights_emergency"   : -1,
-    "show_priv_msg"        : "on",
-    "priv_msg_emergency"   : 0,
     "nick_separator_left"  : "<",
     "nick_separator_right" : "> ",
+    "emergency_hilights"   : "-1",
+    "emergency_priv_msg"   : "0",
+    "activated"            : "on",
+    "show_hilights"        : "on",
+    "show_priv_msg"        : "on",
     "smart_notification"   : "off",
+    "debug"                : "off",
 }
 
-#urgencies = {
+#severity_t = {
     #"emergency" : 2,
     #"high" : 1,
     #"normal" : 0,
@@ -44,11 +34,28 @@ settings = {
     #"low": -2
 #}
 
-# Init everything
+"""
+Init
+"""
+
 for option, default_value in settings.items():
     if weechat.config_get_plugin(option) == "":
         weechat.config_set_plugin(option, default_value)
 
+if weechat.config_get_plugin("apikey") == "":
+    weechat.prnt("", "You haven't set your API key. Use /set plugins.var.python.nma.apikey \"you_nma_api_token\" to fix that.")
+
+
+"""
+Hooks
+"""
+
+# Hook command
+weechat.hook_command("nma", "Activate NotifyMyAndroid notifications", "on | off",
+                      "on : Activate notifications\n"
+                      "off : Desactivate notifications\n",
+                      "on || off",
+                      "nma_cmd_cb", "");
 # Hook privmsg/hilights
 weechat.hook_print("", "irc_privmsg", "", 1, "notify_show", "")
 
@@ -56,10 +63,37 @@ from pynma import PyNMA
 p = PyNMA()
 p.addkey(weechat.config_get_plugin("apikey"))
 
-# Functions
+
+"""
+Helpers
+"""
+
+def _debug(text):
+    if weechat.config_get_plugin("debug") == "on":
+        weechat.prnt("", text)
+
+
+"""
+Functions
+"""
+
+def nma_cmd_cb(data, buffer, args):
+    if args in ["on", "off"]:
+        weechat.prnt("", "Notify My Android notifications %sactivated" 
+                % ("de" if args == "off" else ""))
+        weechat.config_set_plugin('activated', args)
+    else:
+        weechat.prnt("", "Error: Invalid argument")
+        weechat.command("", "/help nma")
+    return weechat.WEECHAT_RC_OK
+
+
 def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed,
         ishilight, prefix, message):
     """Sends highlighted message to be printed on notification"""
+
+    if weechat.config_get_plugin('activated') == "off":
+        return weechat.WEECHAT_RC_OK
 
     if (weechat.config_get_plugin('smart_notification') == "on" and
             bufferp == weechat.current_buffer()):
@@ -74,7 +108,7 @@ def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed,
             weechat.config_get_plugin('show_priv_msg') == "on"):
         ret = show_notification("IRC private message",
         notif_body, int(weechat.config_get_plugin("priv_msg_emergency")))
-        weechat.prnt("", "Message sent: %s. Return: %s." % (notif_body, ret))
+        _debug("", "Message sent: %s. Return: %s." % (notif_body, ret))
 
     # Highlight (your nick is quoted)
     elif (ishilight == "1" and
@@ -83,10 +117,10 @@ def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed,
                 weechat.buffer_get_string(bufferp, "name"))
         ret = show_notification(bufname, notif_body,
                 int(weechat.config_get_plugin("hilights_emergency")))
-        weechat.prnt("", "Message sent: %s. Return: %s." % (notif_body, ret))
+        _debug("Message sent: %s. Return: %s." % (notif_body, ret))
 
     if ret is not None:
-        weechat.prnt("", str(ret))
+        _debug("", str(ret))
 
     return weechat.WEECHAT_RC_OK
 
